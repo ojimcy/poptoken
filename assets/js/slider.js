4b6a049b20c9411d486071dc521eb0ad30b9a01a
ojimcy <ojimcy247@gmail.com>
2023-05-07 00:00:50 +0100

    and and how to buy sections

---
+++ assets/js/slider.js
@@ -0,0 +1,10 @@
+// Initialize Swiper
+var swiper = new Swiper('.swiper-container', {
+    loop: true, // Enable continuous loop
+    slidesPerView: 'auto', // Show as many slides as possible
+    spaceBetween: 10, // Adjust the space between slides as needed
+    autoplay: {
+      delay: 2000, // Adjust the delay between slides as needed
+    },
+  });
+  
